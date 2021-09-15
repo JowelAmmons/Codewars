0306cc24a7ac53a147644a3efda7994abc5c146e
jowel.ammons@gmail.com <89389822+JowelAmmons@users.noreply.github.com>
2021-09-14 22:10:02 -0400

    Level 8 Kyu

---
+++ Transportation_Vacation/main.js
@@ -0,0 +1,19 @@
+/*This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+// PREP
+// Paremeters --> 
+// Return --> 
+// Example --> shoxa -> 
+// Pseudocode -->
+
+*/
+
+function rentalCarCost(d) {
+  if(d > 2 && d <= 6){
+    return total = (d*40) - 20;
+  }else if(d > 6){
+  return total = (d*40) - 50;
+}else {
+return total = (d*40)
+}
+}

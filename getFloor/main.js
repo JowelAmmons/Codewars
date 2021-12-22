cf56d4d68105c39c4931355f99e0327aa6bfbe06
jowel.ammons@gmail.com <89389822+JowelAmmons@users.noreply.github.com>
2021-12-22 11:15:45 -0500

    code war

---
+++ getFloor/main.js
@@ -0,0 +1,10 @@
+// Taking a number as my parameter
+//returning a lesser value then what I was passed in
+
+function getRealFloor(n) {
+  if (n === 0) return n
+   if (n < 0) return n
+   if (n < 13) return n - 1
+   if (n >= 13) return n - 2
+ 
+ }

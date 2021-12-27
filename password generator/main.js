4580d5c664511525d57e959e7b8bdb557103e89b
jowel.ammons@gmail.com <89389822+JowelAmmons@users.noreply.github.com>
2021-12-27 11:16:48 -0500

    code war

---
+++ password generator/main.js
@@ -0,0 +1,17 @@
+/*This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+// PREP
+// Paremeters --> Password is passed through
+// Return --> randomize and create new password
+// Example --> shoxa -> $50*@
+// Pseudocode -->
+Will need to access array and match parameter characters with ones that are in the chars subset
+*/
+
+//These are your super secret characters you will use to make the password super secure
+
+
+var superSecretChars = [['a', '@'],['s', '$'],['o', '0'], ['h', '5'], ['x', '*']];
+function createSSP(password){
+  
+}
